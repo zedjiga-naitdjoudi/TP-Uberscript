baa6731ef9ef024b16b746a8208abbedea2ab730
zedjiga <zedji.naitdjoudi@gmail.com>
2026-03-13 16:53:24 +0100

    show meals

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,21 @@ export type Order = {
   total: number
 }
 
-async function test() {
-    const meals = await fetchMeals()
-    console.log(meals)
+// async function test() {
+//     const meals = await fetchMeals()
+//     console.log(meals)
+// }
+// test()
+
+async function showMeals() {
+  const meals = await fetchMeals()
+  const list = document.getElementById("mealList")
+  meals.forEach(meal => { 
+      const li = document.createElement("li")
+      li.textContent = `${meal.name} - ${meal.price}€`
+      list?.appendChild(li)
+
+    })
+  
 }
-test()
+showMeals()
